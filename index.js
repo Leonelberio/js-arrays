dbfa74b961d635bb42f280f69800006382132c3d
Leandro ADAGBE <67783416+Leonelberio@users.noreply.github.com>
2021-12-03 07:19:51 +0000

    Filter method Added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello")
+let items = [
+  { name: "rice", price: 120 },
+  { name: "maize", price: 200 },
+  { name: "sorgho", price: 300 },
+  { name: "TV", price: 1000 },
+  { name: "Phone", price: 250 },
+  { name: "Laptop", price: 1200 },
+  { name: "Desktop", price: 500 },
+  { name: "Speaker", price: 600 }
+];
+
+//Filter method
+let filteredItems = items.filter((item) => {
+  return item.price < 300;
+});
+
+console.log(filteredItems);
+
+//
